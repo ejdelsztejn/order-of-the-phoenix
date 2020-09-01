26206de57f0dbc868e7df9d9e09c63158b387ebe
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-01 11:54:44 -0400

    Create connection to endpoint

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,9 @@ class SearchController < ApplicationController
   def index
     house = params[:house]
 
-    conn = Faraday.new(url: "https://www.potterapi.com/v1/")
-    
+    conn = Faraday.new(url: "https://www.potterapi.com/v1")
+
+    response = conn.get("https://www.potterapi.com/v1/characters?key=#{ENV['POTTER_API_KEY']}&orderOfThePhoenix=true&house=#{house}")
+    require "pry"; binding.pry
   end
 end
