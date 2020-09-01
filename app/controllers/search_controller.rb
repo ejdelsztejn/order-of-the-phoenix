412760b963f549fbbbe621b754c51c3d4b77d016
Jessye Ejdelman <jessye.ejdelman@gmail.com>
2020-09-01 12:18:04 -0400

    Attempted refactor of conn.get

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,7 @@ class SearchController < ApplicationController
 
     conn = Faraday.new(url: "https://www.potterapi.com/v1")
 
+    # why do I have to put the whole URL in below? Something to investigate
     response = conn.get("https://www.potterapi.com/v1/characters?key=#{ENV['POTTER_API_KEY']}&orderOfThePhoenix=true&house=#{house}")
 
     json = JSON.parse(response.body, symbolize_names: true)
